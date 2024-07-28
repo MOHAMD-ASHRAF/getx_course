18f6e1ae2a4bdba0632d0c8abf9a7ab8545dd5a4
DESKTOP-7C253GF\Eng MoHego <moashrafasd12@gmail.com>
2024-07-28 23:47:40 +0300

    delete first and second screens

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_course/first_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_course/home_screen.dart';
-import 'package:getx_course/second_screen.dart';
 
-import 'first_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,8 +15,6 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       getPages: [
         GetPage(name: "/", page:()=> const HomeScreen()),
-        GetPage(name: "/firstScreen", page:()=> const FirstScreen()),
-        GetPage(name: "/SecondScreen", page:()=> const SecondScreen()),
       ],
       title: 'Flutter Demo',
       theme: ThemeData(
